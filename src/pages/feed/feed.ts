cfbbbb546bbfaba786fd3059680d5dd376fd0b62
Abdirahman Suleman <abdirahmansuleman@Abdirahmans-MacBook-Pro.local>
2018-11-17 15:37:08 -0600

    ionInfinite added and logout button works on feed page

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
+import { NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
 import firebase from 'firebase';
 import moment from 'moment';
-
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-feed',
@@ -13,56 +12,147 @@ export class FeedPage {
 
   text: string = "";
   posts: any[] = [];
+  pageSize: number = 10;
+  cursor: any;
+  infiniteEvent: any;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
+    public toastCtrl: ToastController,
+    public loadingCtrl: LoadingController
     ) {
-
-      this.getPosts();
+    this.getPosts();
   }
 
-  getPosts() {
+  getPosts(){
 
     this.posts = [];
 
-    firebase.firestore().collection("posts").orderBy("created", "desc").get()
+    let loading = this.loadingCtrl.create({
+      content: "Loading Feed..."
+    });
+
+    loading.present();
+
+    let query = firebase.firestore().collection("posts").orderBy("created", "desc").limit(this.pageSize);
+
+    // query.onSnapshot((snapshot) => {
+    //    let changedDocs = snapshot.docChanges();
+
+    //    changedDocs.forEach((change) => {
+    //      if(change.type == "added") {
+    //        //todo
+    //      }
+    //      if(change.type == "modified") {
+    //       //todo
+    //        console.log("document with id " + change.doc.id + " has been modified");
+           
+    //     }
+    //     if(change.type == "removed") {
+    //       //todo
+    //     }
+
+    //    });
+    // })
+    
+    query.get()
     .then((docs) => {
 
       docs.forEach((doc) => {
-        this.posts.push(doc)
+        this.posts.push(doc);
       })
+
+      loading.dismiss();
+
+      this.cursor = this.posts[this.posts.length - 1];
 
       console.log(this.posts)
 
     }).catch((err) => {
-
+      console.log(err)
     })
   }
 
+  loadMorePosts(event){
+
+    firebase.firestore().collection("posts").orderBy("created", "desc").startAfter(this.cursor).limit(this.pageSize).get()
+    .then((docs) => {
+
+      docs.forEach((doc) => {
+        this.posts.push(doc);
+      })
+
+      console.log(this.posts)
+
+      if(docs.size < this.pageSize){
+        // all documents have been loaded
+        event.enable(false);
+        this.infiniteEvent = event;
+      } else {
+        event.complete();
+        this.cursor = this.posts[this.posts.length - 1];
+      }
+
+    }).catch((err) => {
+      console.log(err)
+    })
+
+  }
+
+  refresh(event){
+
+    this.posts = [];
+
+    this.getPosts();
+    
+    if(this.infiniteEvent){
+      this.infiniteEvent.enable(true);
+    }
+
+    event.complete();
+
+  }
+
   post(){
+
     firebase.firestore().collection("posts").add({
       text: this.text,
       created: firebase.firestore.FieldValue.serverTimestamp(),
       owner: firebase.auth().currentUser.uid,
       owner_name: firebase.auth().currentUser.displayName
     }).then((doc) => {
-      console.log(doc);
+      console.log(doc)
 
-      this.getPosts()
-      
+      this.text = "";
+
+      let toast = this.toastCtrl.create({
+        message: "Your post has been created successfully.",
+        duration: 3000
+      }).present();
+
+      this.getPosts();
     }).catch((err) => {
-      console.log(err);
-      
+      console.log(err)
     })
+
   }
 
-  ago(time) { 
-    //converts time param into moment object and can use diff function
-    //calculate the time difference with reference to 
-    //current time in moment is moment as a function 
+  ago(time){
     let difference = moment(time).diff(moment());
     return moment.duration(difference).humanize();
+  }
+
+  logout(){
+    firebase.auth().signOut().then(() => {
+
+      let toast = this.toastCtrl.create({
+        message: " You have been logged out successfully.",
+        duration: 3000
+      }).present();
+      
+      this.navCtrl.setRoot(LoginPage)
+    })
   }
 
 }
