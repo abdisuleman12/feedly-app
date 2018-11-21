e7b4a0c1c7893f26afadb0ac1fe845997e291467
Abdirahman Suleman <abdirahmansuleman@Abdirahmans-MacBook-Pro.local>
2018-11-20 18:11:54 -0600

    added upload percentage to image

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 import firebase from 'firebase';
 import moment from 'moment';
 import { LoginPage } from '../login/login';
@@ -18,17 +18,11 @@ export class FeedPage {
   infiniteEvent: any;
   image: string;
 
-  constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams,
-    public toastCtrl: ToastController,
-    public loadingCtrl: LoadingController,
-    private camera: Camera
-    ) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private loadingCtrl: LoadingController, private toastCtrl: ToastController, private camera: Camera) {
     this.getPosts();
   }
 
-  getPosts(){
+  getPosts() {
 
     this.posts = [];
 
@@ -41,75 +35,75 @@ export class FeedPage {
     let query = firebase.firestore().collection("posts").orderBy("created", "desc").limit(this.pageSize);
 
     // query.onSnapshot((snapshot) => {
-    //    let changedDocs = snapshot.docChanges();
+    //   let changedDocs = snapshot.docChanges();
 
-    //    changedDocs.forEach((change) => {
-    //      if(change.type == "added") {
-    //        //todo
-    //      }
-    //      if(change.type == "modified") {
-    //       //todo
-    //        console.log("document with id " + change.doc.id + " has been modified");
-           
-    //     }
-    //     if(change.type == "removed") {
-    //       //todo
+    //   changedDocs.forEach((change) => {
+    //     if(change.type == "added"){
+    //       // TODO
     //     }
 
-    //    });
+    //     if(change.type == "modified"){
+    //       // TODO
+    //       console.log("Document with id " + change.doc.id + " has been modified.");
+    //     }
+
+    //     if(change.type == "removed"){
+    //       // TODO
+    //     }
+    //   })
     // })
-    
+
     query.get()
-    .then((docs) => {
+      .then((docs) => {
 
-      docs.forEach((doc) => {
-        this.posts.push(doc);
+        docs.forEach((doc) => {
+          this.posts.push(doc);
+        })
+
+        loading.dismiss();
+
+        this.cursor = this.posts[this.posts.length - 1];
+
+        console.log(this.posts)
+
+      }).catch((err) => {
+        console.log(err)
       })
-
-      loading.dismiss();
-
-      this.cursor = this.posts[this.posts.length - 1];
-
-      console.log(this.posts)
-
-    }).catch((err) => {
-      console.log(err)
-    })
   }
 
-  loadMorePosts(event){
+  loadMorePosts(event) {
 
     firebase.firestore().collection("posts").orderBy("created", "desc").startAfter(this.cursor).limit(this.pageSize).get()
-    .then((docs) => {
+      .then((docs) => {
 
-      docs.forEach((doc) => {
-        this.posts.push(doc);
+        docs.forEach((doc) => {
+          this.posts.push(doc);
+        })
+
+        console.log(this.posts)
+
+        if (docs.size < this.pageSize) {
+          // all documents have been loaded
+          event.enable(false);
+          this.infiniteEvent = event;
+        } else {
+          event.complete();
+          this.cursor = this.posts[this.posts.length - 1];
+        }
+
+      }).catch((err) => {
+        console.log(err)
       })
-
-      console.log(this.posts)
-
-      if(docs.size < this.pageSize){
-        // all documents have been loaded
-        event.enable(false);
-        this.infiniteEvent = event;
-      } else {
-        event.complete();
-        this.cursor = this.posts[this.posts.length - 1];
-      }
-
-    }).catch((err) => {
-      console.log(err)
-    })
 
   }
 
-  refresh(event){
+  refresh(event) {
 
     this.posts = [];
 
     this.getPosts();
-    
-    if(this.infiniteEvent){
+
+    if (this.infiniteEvent) {
       this.infiniteEvent.enable(true);
     }
 
@@ -117,7 +111,7 @@ export class FeedPage {
 
   }
 
-  post(){
+  post() {
 
     firebase.firestore().collection("posts").add({
       text: this.text,
@@ -127,7 +121,7 @@ export class FeedPage {
     }).then(async (doc) => {
       console.log(doc)
 
-      if(this.image){
+      if (this.image) {
         await this.upload(doc.id)
       }
 
@@ -146,28 +140,32 @@ export class FeedPage {
 
   }
 
-  ago(time){
+  ago(time) {
     let difference = moment(time).diff(moment());
     return moment.duration(difference).humanize();
   }
 
-  logout(){
+  logout() {
+
     firebase.auth().signOut().then(() => {
 
       let toast = this.toastCtrl.create({
-        message: " You have been logged out successfully.",
+        message: "You have been logged out successfully.",
         duration: 3000
       }).present();
 
-      this.navCtrl.setRoot(LoginPage)
-    })
+      this.navCtrl.setRoot(LoginPage);
+    });
+
   }
 
-  addPhoto(){
+  addPhoto() {
+
     this.launchCamera();
+
   }
 
-  launchCamera(){
+  launchCamera() {
     let options: CameraOptions = {
       quality: 100,
       sourceType: this.camera.PictureSourceType.CAMERA,
@@ -181,47 +179,68 @@ export class FeedPage {
     }
 
     this.camera.getPicture(options).then((base64Image) => {
-      console.log(base64Image)
+      console.log(base64Image);
 
       this.image = "data:image/png;base64," + base64Image;
+
 
     }).catch((err) => {
       console.log(err)
     })
   }
 
-  upload(name: string){
+  upload(name: string) {
 
-    return new Promise ((reject, resolve) => {
+    return new Promise((resolve, reject) => {
+
+      let loading = this.loadingCtrl.create({
+        content: "Uploading Image..."
+      })
+
+      loading.present();
+
       let ref = firebase.storage().ref("postImages/" + name);
 
       let uploadTask = ref.putString(this.image.split(',')[1], "base64");
-  
-      uploadTask.on("state_changed", (taskSnapshot) => {
+
+      uploadTask.on("state_changed", (taskSnapshot: any) => {
         console.log(taskSnapshot)
+
+        let percentage = taskSnapshot.bytesTransferred / taskSnapshot.totalBytes * 100;
+
+        loading.setContent("Uploaded " + percentage + "% ...")
+
+
       }, (error) => {
         console.log(error)
-      }, () =>{
+      }, () => {
         console.log("The upload is complete!");
-  
+
         uploadTask.snapshot.ref.getDownloadURL().then((url) => {
-          
+
           firebase.firestore().collection("posts").doc(name).update({
             image: url
           }).then(() => {
+            loading.dismiss()
             resolve()
+            
           }).catch((err) => {
+            loading.dismiss()
             reject()
+    
           })
 
-        }).catch(() => {
+        }).catch((err) => {
+          loading.dismiss()
           reject()
         })
-  
+
       })
+
     })
 
-  }
 
+
+  }
 
 }
